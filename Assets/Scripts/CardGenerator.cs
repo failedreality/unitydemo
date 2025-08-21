55520cbf7b637bba89333ab9606661e9c07a7189
failedreality <jsweber@gmail.com>
2025-08-21 13:52:06 -0500

    Testing adding script to Unity

---
+++ Assets/Scripts/CardGenerator.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CardGenerator : MonoBehaviour  //*use this when in Unity environment*
+{
+    public List<Cards> generatedCards = new List<Cards>();
+
+    void Start()  //void Start() // Use Start() when in Unity environment
+    {
+        CardGenerator generator = new CardGenerator(); // Create an instance
+
+        System.Random rand = new System.Random();
+        for (int i = 1; i <= 10; i++)
+        {
+            int attack = rand.Next(1, 11); // Random attack value between 1 and 10
+            int defense = rand.Next(1, 11); // Random defense value between 1 and 10
+            int hitPoints = 21; // Fixed hit points value
+            /*
+            Not sure how to determine the energy cost quite yet.
+            So for now leaving what I have in place below.
+            */
+            int energyCost = attack * 2 + defense * 2 * hitPoints; //This will need some readjustment.
+
+            Cards card = new Cards
+            {
+                attack = attack,
+                defense = defense,
+                hitPoints = hitPoints,
+                energyCost = energyCost
+            };
+
+            generator.generatedCards.Add(card); // Use the instance to access generatedCards
+            Debug.Log(generatedCards.Count); // Log the count of generated cards
+
+            Console.WriteLine($"Card {i}: {card}");
+        }
+    }
+}
